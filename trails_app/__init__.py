ac4b3689afb0a055c56b1bca839e42b0c92837ee
Michele Alberti <62114934+Michele-Alberti@users.noreply.github.com>
2022-05-07 23:04:56 +0200

    feat: add support for hydra framework
    app configuration is now handled with hydra facilities, add configuration folder to the project

--- trails_app/__init__.py
+++ trails_app/__init__.py
@@ -6,6 +6,7 @@ from flask_login import LoginManager
 from flask_wtf.csrf import CSRFProtect
 from pathlib import Path
 import os
+from omegaconf import DictConfig, open_dict
 
 # Database imports
 from . import models
@@ -19,24 +20,12 @@ from .main import main as main_blueprint
 # APP FACTORY FUNCTION --------------------------------------------------------
 
 
-def create_app(test_config=None):
+def create_app(config: DictConfig):
     """Flask app factory function"""
 
     # Create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY="dev",
-        DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
-        SQLALCHEMY_DATABASE_URI="sqlite:///trails.db",
-        SQLALCHEMY_TRACK_MODIFICATIONS=False,
-    )
-
-    if test_config is None:
-        # Load the instance config, if it exists, when not testing
-        app.config.from_pyfile("config.py", silent=True)
-    else:
-        # Load the test config if passed in
-        app.config.from_mapping(test_config)
+    app.config.from_object(config.flask)
 
     # Ensure the instance folder exists
     Path(app.instance_path).mkdir(parents=True, exist_ok=True)
