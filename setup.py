74a974f7d9eaf8f57ab6782d1271de4a1994f7d0
Michele Alberti <62114934+Michele-Alberti@users.noreply.github.com>
2022-05-21 20:03:48 +0200

    release 0.4.1 (previous release 0.4.0)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="trails_cli",
-    version="0.4.0",
+    version="0.4.1",
     py_modules=["trails_app"],
     install_requires=[
         "flask==2.0.3",
--- trails_app/__init__.py
+++ trails_app/__init__.py
@@ -1,5 +1,5 @@
 # App metadata
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 
 import logging
 from flask import Flask
